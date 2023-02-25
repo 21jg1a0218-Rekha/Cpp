0a61a2dc0c0c627801f2b03754fc6fd37c185732
21jg1a0218-Rekha <124431424+21jg1a0218-Rekha@users.noreply.github.com>
2023-02-25 12:52:29 +0530

    Add files via upload

---
+++ zig_zag.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+void(zig_zag_array(int arr[],int n))
+{
+    int i;
+    bool ans=true;
+    for(i=0;i<=n-2;i++)
+    {
+    if(ans)
+    {
+       if(arr[i]>arr[i+1])
+          swap(arr[i],arr[i+1]);
+    }
+    else
+    {
+        if(arr[i]<arr[i+1])
+           swap(arr[i],arr[i+1]);
+    }
+    ans=!ans;
+    }
+}
+int main()
+{
+     int n,i;
+    cout<<"enter the value of n:";
+    cin>>n;
+    int arr[n];
+    cout<<"enter the elements in the array:";
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    zig_zag_array(arr,n);
+    cout<<"\n New array elements:";
+    for(i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
