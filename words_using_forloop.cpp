70598d9223051b15d642577c9b87f25a59ed404d
21jg1a0218-Rekha <124431424+21jg1a0218-Rekha@users.noreply.github.com>
2023-08-06 00:24:01 +0530

    Add files via upload

---
+++ words_using_forloop.cpp
@@ -0,0 +1,32 @@
+/*For each integer  in the inclusive interval :
+
+If ,1<=n<=9 then print the English representation of it in lowercase. That is "one" for , "two" for , and so on.
+Else if n>9 and it is an even number, then print "even".
+Else if n>9 and it is an odd number, then print "odd".*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    string arr[9]={"one","two","three","four","five","six","seven","eight","nine"};
+    for(int i=a;i<=b;i++)
+    {
+        if(i<=9)
+        {
+            cout<<arr[i-1]<<endl;
+        }
+        else
+        {
+            if(i%2==0)
+            {
+                cout<<"even"<<endl;
+            }
+            else
+            {
+                cout<<"odd"<<endl;
+            }
+        }
+    }
+    return 0;
+}
